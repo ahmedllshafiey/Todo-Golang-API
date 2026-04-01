c36770e955a40b7a4ae922021aee5772e49a8dfc
ahmedllshafiey <ahmedll.shafiey@gmail.com>
2026-04-01 19:19:48 +0200

    edit: Change entry point to connect Database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"log"
+	"todo-api/internal/config"
+	"todo-api/internal/database"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+	var cfg *config.Config
+	var err error
+
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failed to load connect to database:", err)
+	}
+
+	defer pool.Close()
+
 	var router *gin.Engine = gin.Default()
 	router.SetTrustedProxies(nil)
 	router.GET("/", func(c *gin.Context) {
 		// gin.H => map[string]interface{}
 		// gin.H => map[string]any{}
 		c.JSON(200, gin.H{
-			"message": "Todo API is running",
-			"status":  "success",
+			"message":  "Todo API is running",
+			"status":   "success",
+			"database": "connected",
 		})
 	})
 
-	router.Run(":3000")
+	router.Run(":" + cfg.Port)
 }
