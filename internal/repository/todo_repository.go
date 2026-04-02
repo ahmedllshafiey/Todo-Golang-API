4e1d0c09a5b3cedfafcb2158a01b31aa8da21499
ahmedllshafiey <ahmedll.shafiey@gmail.com>
2026-04-02 18:06:25 +0200

    add: Deleting todo functionality added

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"time"
 	"todo-api/internal/models"
 
@@ -145,4 +146,30 @@ func UpdateTodo(pool *pgxpool.Pool, id int, title string, completed bool) (*mode
 	}
 
 	return &todo, nil
+}
+
+func DeleteToDo(pool *pgxpool.Pool, id int) error {
+	var ctx context.Context
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	var query string = `
+		DELETE FROM todos
+		WHERE id = $1
+	`
+
+	var commandTag, err = pool.Exec(ctx, query, id)
+
+	if err != nil {
+		return err
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return fmt.Errorf("Todo with id %d not found, id")
+	}
+
+	return nil
 }
